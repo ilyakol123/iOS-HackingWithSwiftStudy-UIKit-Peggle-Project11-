0ce6a71b2b74cf8a091a58427775b1973ab73014
Kolesnikov Ilya <ilja-kolesnikov@mail.ru>
2025-02-25 19:49:59 +0300

    Added part 1-3

--- Peggle(Project11)/GameViewController.swift
+++ Peggle(Project11)/GameViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
 
@@ -16,7 +15,7 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+            if let scene = GameScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
@@ -30,6 +29,10 @@ class GameViewController: UIViewController {
             view.showsNodeCount = true
         }
     }
+    
+    override var shouldAutorotate: Bool {
+            return true
+        }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
